8150b3b026db5d5904f85ed73a7726c04f4fbe1c
Marcin <marcinrszalek@gmail.com>
2017-05-23 20:51:03 +0200

    5com

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var newGameBtn = document.getElementById('js-newGameButton');
 var pickRock = document.getElementById('js-playerPick_rock'),
     pickPaper = document.getElementById('js-playerPick_paper'),
     pickScissors = document.getElementById('js-playerPick_scissors');
-var newGameElem = document.getElementById('js-newGameElement'),
-    pickElem = document.getElementById('js-playerPickElement'),
-    resultsElem = document.getElementById('js-resultsTableElement');
 var playerPointsElem = document.getElementById('js-playerPoints'),
     playerNameElem = document.getElementById('js-playerName'),
     computerPointsElem = document.getElementById('js-computerPoints');
@@ -21,12 +18,17 @@ var gameState = 'notStarted',  //started // ended
     computer = {
         score: 0
     };
+var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
 
 newGameBtn.addEventListener('click', newGame);
 pickRock.addEventListener('click', function() { playerPick('rock') });
 pickPaper.addEventListener('click', function() { playerPick('paper') });
 pickScissors.addEventListener('click', function() { playerPick('scissors') });
 
+setGameElements();
 
 function setGameElements() {
     switch(gameState) {
@@ -45,20 +47,21 @@ function setGameElements() {
     }
 }
 
-
 function newGame() {
     player.name = prompt('Graczu, wpisz swoje imię', 'imię gracza');
     if (player.name) {
         player.score = computer.score = 0;
         gameState = 'started';
         setGameElements();
-        setGamePoints();
 
         playerNameElem.innerHTML = player.name;
-
+        setGamePoints();
     }
 
 }
+
+
+
 function setGamePoints() {
     playerPointsElem.innerHTML = player.score;
     computerPointsElem.innerHTML = computer.score;
